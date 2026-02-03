635fbec5eda6a8aabea76a8040ea4c72fbcbdfb1
Aman <workemailataniyazov.a@gmail.com>
2026-02-03 21:49:29 +0500

    Обновил валидацию данных (схем) в файле schemas.py

--- backend/_source/schemas.py
+++ backend/_source/schemas.py
@@ -2,19 +2,22 @@ from pydantic import BaseModel, Field
 
 # --- Создание задачи ---
 class TaskCreate(BaseModel):
-    title: str = Field(None, min_length=3, max_length=50, description="Название задачи") 
-    description: str | None = Field(None, min_length=3, max_length=100, description="Описание задачи")
-    done: bool = Field(False, description="Статус задачи")
+    # -- Расширенная валидация данных -- 
+    title: str = Field(..., min_length=3, max_length=50, description="Название задачи") # ... обязательное поле 
+    description: str | None = Field(None, min_length=3, max_length=100, description="Описание задачи") 
+    done: bool = Field(False, description="Статус задачи") # по умолчанию задача не выполнена
 
 # --- Обновление задачи ---
 class TaskUpdate(BaseModel):
-    title: str | None = Field(None, min_length=3, max_length=50) 
+    title: str | None = Field(None, min_length=3, max_length=50)
     description: str | None = Field(None, max_length=100)
-    done: bool | None = None
+    done: bool | None = None # булевое значение или отсутствует
 
 # --- Ответ сервера ---
 class TaskOut(BaseModel):
-    id: int
-    title: str
-    description: str | None
-    done: bool 
+    # -- Принимает в формате --
+    # целового числа, строки, строки с значением отсутствует и булевое значение 
+    id: int # целое число
+    title: str # строка
+    description: str | None # строка и значение отсутствует
+    done: bool # булевое значение true/false
