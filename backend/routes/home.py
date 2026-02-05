22d3c1190179da52e1ada0ab164199f77798ac57
Aman <workemailataniyazov.a@gmail.com>
2026-02-06 00:51:17 +0500

    Изменил home.py в schemas и router папках валидация данных и описании CRUD оператора

--- backend/routes/home.py
+++ backend/routes/home.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from backend.schemas.home import HomeRootResponse, HomeAboutResponse
 from backend.routes.task import get_tasks_count
 from backend.routes.pomodoro import get_pomodoro_progress
 from backend.routes.click import get_click_glasses
