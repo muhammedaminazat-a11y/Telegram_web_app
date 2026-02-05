c65d19f833f1f2dfa98008381f96ae6c7a45aa94
Aman <workemailataniyazov.a@gmail.com>
2026-02-05 10:25:59 +0500

    Обновил home.py эндпоинт главной страницы через импорт от других модулей из папки routes

--- backend/routes/home.py
+++ backend/routes/home.py
@@ -1,26 +1,36 @@
 from fastapi import APIRouter
+from routes.tasks import get_tasks_count
+from routes.pomodoro import get_pomodoro_progress
+from routes.click import get_click_glasses
+from routes.games import get_games_glasses
+
 
 router = APIRouter(
     prefix="/home",
     tags=["home"]
     )
 
-# эндпоинт получения домашней страницы (заглушка)
-@router.get("/")
-def get_home():
-    return {"message": "Главная страница"}
+# эндпоинт приветственное сообщение
+@router.get("/", summary="Главная страница", description="Приветствие и краткая статистика")
+def read_root(user_id: int = 1): # user_id: - параметр, int - должны быть только целые числа 
+    # | Если user_id не будет передан, то всегда будет = 1 |
+    return {"message": "Привет, это главная страница!",
+            "tasks_count": get_tasks_count(),
+            "pomodoro_progress": get_pomodoro_progress(),
+            "click_glasses": get_click_glasses(user_id),
+            "games_glasses": get_games_glasses(user_id)
+            }
 
-# эндпоинт создание домашней страницы (заглушка)
-@router.post("/")
-def create_home():
-    return {"message": "Главная страница создана"}
-
-# эндпоинт обновления домашней страницы (заглушка)
-@router.put("/{home_id}")
-def update_home(home_id: int):
-    return {"message": f"Главная страница {home_id} обновлена"}
-
-# эндпоинт удаление домашней страницы (заглушка)
-@router.delete("/{home_id}")
-def delete_home(home_id: int):
-    return {"message": f"Главная страница {home_id} удалена"}
+# эндпоинт приветствие и краткая статистика
+@router.get("/about")
+def read_about(user_id: int = 1): 
+    return {
+        "service": "Приложение для повышения производительности",
+        "description": "Количество задач, Метод Pomodoro, игры и кликер",
+        "stats": {
+            "tasks_count": get_tasks_count(),
+            "pomodoro_progress": get_pomodoro_progress(),
+            "click_glasses": get_click_glasses(user_id),
+            "games_glasses": get_games_glasses(user_id)
+        }
+    }
