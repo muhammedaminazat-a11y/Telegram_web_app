5cbfb7a6e5772fec32cbac6b4193c4ec2cbec550
Aman <workemailataniyazov.a@gmail.com>
2026-02-07 02:21:48 +0500

    Добавил для сущности ai роутеров 3 файла и добавил в них заглушки

--- backend/routes/ai.py
+++ backend/routes/ai.py
@@ -1,11 +1,19 @@
 from fastapi import APIRouter, HTTPException
+from backend.services import ai_service
 from backend.schemas.ai import (
-    AiCreate, 
-    AiUpdate,
-    AiOut,
+    AIRequest, 
+    AIResponse
 )
 
 router = APIRouter(
     prefix="/ai",
     tags=["ai"]
-    )
+)
+
+@router.post("/chat", response_model=AIResponse)
+def ai_chat(request: AIRequest):
+    try:
+        answer = ai_service.ask_ai(request.prompt)
+        return AIResponse(answer=answer)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
