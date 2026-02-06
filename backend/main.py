d7144e2716efc78bd4c414f74761a5da1d0eb082
Aman <workemailataniyazov.a@gmail.com>
2026-02-07 02:32:05 +0500

    Обновил подключение роутеров для задач их место как они стоят в проекте

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,8 @@ from backend.routes import click, ai, home, pomodoro, profile, task
 app = FastAPI(title="Telegram Mini App To Do List API")
 
 # подключение роутера для задач
-app.include_router(click.router)
 app.include_router(ai.router)
+app.include_router(click.router)
 app.include_router(home.router)
 app.include_router(pomodoro.router)
 app.include_router(profile.router)
