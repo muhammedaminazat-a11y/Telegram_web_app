6afb0118f79ac0803093106dd346cd3e00eefa4a
Aman <workemailataniyazov.a@gmail.com>
2026-02-07 17:25:43 +0500

    Добавил CORSMiddleware, чтобы работало при разных доменов frontend и backend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.routes import ai, click, home, pomodoro, profile, task
 
 # создания экземпляра приложения FastAPI
 app = FastAPI(title="Telegram Mini App To Do List API")
+
+# подключение к CORS (чтобы работать на разных доменах frontend и backend)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # подключение роутера для задач
 app.include_router(ai.router)
