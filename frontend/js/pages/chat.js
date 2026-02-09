ebdfc851014b0ddb26ebde3a4eaecbdb7230a8f5
Maksim <muhammedaminazat@gmail.com>
2026-02-09 23:25:25 +0500

    исправлене ошибки

--- frontend/js/pages/chat.js
+++ frontend/js/pages/chat.js
@@ -1,4 +1,4 @@
-const API_BASE = "const API_BASE = "http://localhost:8000";
+const API_BASE = "http://localhost:8000";
 
 export function initChat() {
   const box = document.getElementById("chatBox");
