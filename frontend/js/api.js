347c63d88b3024b4da5434a946ce2953028de520
Aman <workemailataniyazov.a@gmail.com>
2026-02-10 09:39:40 +0500

    BB

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = window.API_BASE || "http://localhost:8000";
+const API_BASE = window.API_BASE || "http://10.171.7.173:8000";
 
 async function request(path, options = {}) {
   const res = await fetch(`${API_BASE}${path}`, {
