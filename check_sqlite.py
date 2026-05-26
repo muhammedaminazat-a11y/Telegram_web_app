f0f1bb190771014cf75b75f268bf78776719988d
Aman <workemailataniyazov.a@gmail.com>
2026-05-26 18:08:34 +0500

    Удалил не нужный файл созданный для теста проверки при переходе с PostgreSQL на SQLite базу данных

--- check_sqlite.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("test.db")
-cursor = conn.cursor()
-
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-print("Таблицы:", cursor.fetchall())
-
-conn.close()
